4ef8152c8abd419821756b32d5cf2f8d55b156c3
Ícaro Sanches <104044375+igfsa@users.noreply.github.com>
2022-09-12 22:04:53 -0300

    Adição de funções de seleção ao filtro, adição de opções na tabela

--- front/proeventos-app/src/app/eventos/eventos.component.ts
+++ front/proeventos-app/src/app/eventos/eventos.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
+import { ConnectableObservable } from 'rxjs';
 
 @Component({
   selector: 'app-eventos',
@@ -20,12 +21,20 @@ export class EventosComponent implements OnInit {
   margemImagem: number = 2;
   exibirImagem: boolean = true;
 
-  filtraId: boolean = true;
-  filtraTema: boolean = true;
-  filtraLocal: boolean = true;
-  filtraData: boolean = true;
-  filtraQtd: boolean = true;
-  filtraLote: boolean = true;
+  private _filtraId: boolean = false;
+  private _filtraTema: boolean = true;
+  private _filtraLocal: boolean = true;
+  private _filtraData: boolean = true;
+  private _filtraQtd: boolean = true;
+  private _filtraLote: boolean = true;
+  // As variáveis privadas são criadas devida a necessidade de getters e setters para conseguir fazer a interpolação. Caso não seja feita a configuração dos getters e setters, o valor não é corretamente passado para a variável
+
+  Id: string = '';
+  Tema: string = '';
+  Local: string = '';
+  Data: string = '';
+  Qtd: string = '';
+  Lote: string = '';
 
   private _filtroLista: string = '';
 
@@ -39,10 +48,70 @@ export class EventosComponent implements OnInit {
     // Não podemos atribuir this.eventos diretamente para this.eventos pois ao usarmos o filtro eventos assume um valor, e apos ser utilizado, sera retornado esse mesmo valor
   }
 
-  filtrarEventos(filtraPor: string) : any
-  {
-    filtraPor = filtraPor.toLocaleLowerCase();
+  public get filtraId(): boolean{
+    return this._filtraId;
+  }
 
+  public set filtraId(valor: boolean){
+    this._filtraId = valor;
+  }
+
+  public get filtraTema(): boolean{
+    return this._filtraTema;
+  }
+
+  public set filtraTema(valor: boolean){
+    this._filtraTema = valor;
+  }
+
+  public get filtraLocal(): boolean{
+    return this._filtraLocal;
+  }
+
+  public set filtraLocal(valor: boolean){
+    this._filtraLocal = valor;
+  }
+
+  public get filtraData(): boolean{
+    return this._filtraData;
+  }
+
+  public set filtraData(valor: boolean){
+    this._filtraData = valor;
+  }
+
+  public get filtraQtd(): boolean{
+    return this._filtraQtd;
+  }
+
+  public set filtraQtd(valor: boolean){
+    this._filtraQtd = valor;
+  }
+
+  public get filtraLote(): boolean{
+    return this._filtraLote;
+  }
+
+  public set filtraLote(valor: boolean){
+    this._filtraLote = valor;
+  }
+
+  private _tipoFiltro: any;
+
+  public get tipoFiltro(): tipoFiltro{
+    return this._tipoFiltro = new tipoFiltro(this.filtraId, this.filtraTema, this.filtraLocal, this.filtraData, this.filtraQtd, this.filtraLote);
+  }
+
+  public set tipoFiltro(valor: tipoFiltro){
+    this._tipoFiltro = valor;
+  }
+
+
+
+
+  public filtrarEventos(filtrarPor: string) : any
+  {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter
     (
       (evento:
@@ -54,15 +123,16 @@ export class EventosComponent implements OnInit {
           lote: string;
           eventoId: string;
         }) =>(
-            evento.tema.toLocaleLowerCase().indexOf(filtraPor) !== -1
-            || evento.local.toLocaleLowerCase().indexOf(filtraPor) !== -1
-            || evento.dataEvento.toLocaleLowerCase().indexOf(filtraPor) !== -1
-            || evento.qtdPessoas.toString().toLocaleLowerCase().indexOf(filtraPor) !== -1
-            || evento.lote.toString().toLocaleLowerCase().indexOf(filtraPor) !== -1
-            || evento.eventoId.toString().toLocaleLowerCase().indexOf(filtraPor) !== -1
+            (this.tipoFiltro.filtraTema ? evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 : 0)
+            || (this.tipoFiltro.filtraLocal ? evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 : 0)
+            || (this.tipoFiltro.filtraData ? evento.dataEvento.toLocaleLowerCase().indexOf(filtrarPor) !== -1 : 0)
+            || (this.tipoFiltro.filtraQtd ? evento.qtdPessoas.toString().toLocaleLowerCase().indexOf(filtrarPor) !== -1 : 0)
+            || (this.tipoFiltro.filtraLote ? evento.lote.toString().toLocaleLowerCase().indexOf(filtrarPor) !== -1 : 0)
+            || (this.tipoFiltro.filtraId ? evento.eventoId.toString().toLocaleLowerCase().indexOf(filtrarPor) !== -1 : 0)
         )
     )
   }
+
 
   constructor(private http: HttpClient) { }
 
@@ -71,7 +141,11 @@ export class EventosComponent implements OnInit {
   }
 
   alterarImagem(){
-    this.exibirImagem = !this.exibirImagem
+    this.exibirImagem = !this.exibirImagem;
+  }
+
+  clickFiltrarEventos(){
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
   public GetEventos (): void{
@@ -104,3 +178,24 @@ export class EventosComponent implements OnInit {
     // ]
   }
 }
+
+class tipoFiltro
+{
+  filtraId: boolean;
+  filtraTema: boolean;
+  filtraLocal: boolean;
+  filtraData: boolean;
+  filtraQtd: boolean;
+  filtraLote: boolean;
+
+  constructor(filtraId: boolean, filtraTema: boolean, filtraLocal: boolean, filtraData: boolean, filtraQtd: boolean, filtraLote: boolean)
+  {
+    this.filtraId = filtraId;
+    this.filtraTema = filtraTema;
+    this.filtraLocal = filtraLocal;
+    this.filtraData = filtraData;
+    this.filtraQtd = filtraQtd;
+    this.filtraLote = filtraLote;
+  }
+}
+
